a1ad5f54307aa6995c5e44a021badadce351aa0b
Paul Becker <paube@mail.upb.de>
2019-11-09 18:27:38 +0100

    Functional Log4j Logger, Add Java Applications for Intellij, Add Guice Dependency for Server component

--- src/main/java/de/upb/codingpirates/battleships/server/ServerMain.java
+++ src/main/java/de/upb/codingpirates/battleships/server/ServerMain.java
@@ -1,6 +1,13 @@
 package de.upb.codingpirates.battleships.server;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ServerMain {
+
+    private static final Logger LOGGER = LogManager.getLogger();
     public static void main(String[] args) {
+        LOGGER.info("start");
     }
 }
--- src/test/java/de/upb/codingpirates/battleships/server/test/ServerTests.java
+++ src/test/java/de/upb/codingpirates/battleships/server/test/ServerTests.java
@@ -1,12 +1,15 @@
 package de.upb.codingpirates.battleships.server.test;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 public class ServerTests {
 
+    private static final Logger LOGGER = LogManager.getLogger();
     @Test
     public void test(){
-
+        LOGGER.info("test started");
     }
 }
