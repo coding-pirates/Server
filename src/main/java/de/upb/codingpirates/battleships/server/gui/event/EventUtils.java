89c72880b962affc14fe4684b336953e64f162be
Andre Blanke <blanke.and@gmail.com>
2019-11-20 13:32:35 +0100

    Improve documentation

--- src/main/java/de/upb/codingpirates/battleships/server/gui/event/EventUtils.java
+++ src/main/java/de/upb/codingpirates/battleships/server/gui/event/EventUtils.java
@@ -1,6 +1,7 @@
 package de.upb.codingpirates.battleships.server.gui.event;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventTarget;
 import javafx.scene.Node;
 import javafx.stage.Window;
 
@@ -14,7 +15,22 @@ public final class EventUtils {
     private EventUtils() {
     }
 
+    /**
+     * Retrieves the {@link Window} associated with the {@code event}'s {@link EventTarget},
+     * provided the target is of tye {@link Node}, otherwise throws an
+     * {@link UnsupportedOperationException}.
+     *
+     * @param event The event whose associated {@link Window} is to be retrieved.
+     *
+     * @return The {@link Window} associated with the {@code event}'s {@link EventTarget}.
+     *
+     * @throws UnsupportedOperationException If the {@link EventTarget} is not a {@link Node}.
+     */
     public static Window getWindowFromTarget(@NotNull final ActionEvent event) {
-        return ((Node) event.getTarget()).getScene().getWindow();
+        final EventTarget target = event.getTarget();
+
+        if (target instanceof Node)
+            return ((Node) event.getTarget()).getScene().getWindow();
+        throw new UnsupportedOperationException("EventTarget not a Node.");
     }
 }
