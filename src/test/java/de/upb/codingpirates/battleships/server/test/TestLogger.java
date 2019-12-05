c1449cc931f5fb073a7b137fc4ce890aec5ec869
Paul Becker <paube@mail.upb.de>
2019-12-05 23:05:32 +0100

    remove TestLogger

--- src/test/java/de/upb/codingpirates/battleships/server/test/TestLogger.java
+++
@@ -1,8 +0,0 @@
-package de.upb.codingpirates.battleships.server.test;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public interface TestLogger {
-    Logger LOGGER = LogManager.getLogger();
-}
