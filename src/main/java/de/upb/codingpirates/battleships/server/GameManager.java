baa27c4448cca90a27a347704e9c98092c1f3a20
bkasten <bkasten@mail.upb.de>
2020-01-07 09:56:29 +0100

    testing shotPlace3 -- something doesnt work

--- src/main/java/de/upb/codingpirates/battleships/server/GameManager.java
+++ src/main/java/de/upb/codingpirates/battleships/server/GameManager.java
@@ -102,7 +102,7 @@ public class GameManager {
             throw new InvalidActionException("game.gameManager.noGame");
         }
         //Todo remove call
-        if (this.getGameHandler(gameId).getAllClients().size() == 3){
+        if (this.getGameHandler(gameId).getAllClients().size() == 2){
             launchGame(gameId);
         }
     }
