bd1a7812e832272b78521e43cde97d38b39ba0f2
Paul Becker <paube@mail.upb.de>
2020-01-13 16:33:24 +0100

    SpectatorGameStateRequestHandler returns empty ships if still in pre gameState

--- src/main/java/de/upb/codingpirates/battleships/server/handler/SpectatorGameStateRequestHandler.java
+++ src/main/java/de/upb/codingpirates/battleships/server/handler/SpectatorGameStateRequestHandler.java
@@ -1,7 +1,9 @@
 package de.upb.codingpirates.battleships.server.handler;
 
+import com.google.common.collect.ImmutableMap;
 import de.upb.codingpirates.battleships.logic.ClientType;
-import de.upb.codingpirates.battleships.logic.GameState;
+import de.upb.codingpirates.battleships.logic.GameStage;
+import de.upb.codingpirates.battleships.logic.PlacementInfo;
 import de.upb.codingpirates.battleships.network.exceptions.game.GameException;
 import de.upb.codingpirates.battleships.network.exceptions.game.NotAllowedException;
 import de.upb.codingpirates.battleships.network.id.Id;
@@ -13,8 +15,11 @@ import de.upb.codingpirates.battleships.server.game.GameHandler;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.Map;
 
 public final class SpectatorGameStateRequestHandler extends AbstractServerMessageHandler<SpectatorGameStateRequest> {
+
+    private static final Map<Integer, Map<Integer, PlacementInfo>> EMPTY = ImmutableMap.of();
 
     @Inject
     public SpectatorGameStateRequestHandler(@Nonnull final ClientManager clientManager, @Nonnull final GameManager gameManager) {
@@ -32,7 +37,7 @@ public final class SpectatorGameStateRequestHandler extends AbstractServerMessag
             ResponseBuilder.spectatorGameStateResponse()
                 .players(handler.getPlayers())
                 .shots(handler.getShots())
-                .ships(handler.getStartShip())
+                .ships(handler.getStage().equals(GameStage.PLACESHIPS) || handler.getStage().equals(GameStage.START) ? EMPTY : handler.getStartShip())
                 .gameState(handler.getGame().getState())
                 .build(),
             connectionId);
